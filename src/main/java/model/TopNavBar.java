1f25e7042cd8ef2721e0d35a9d42957b3674b754
TolkachDima <tolkach.dima64@gmail.com>
2020-12-10 21:41:52 +0300

    Refactoring
    * Changed the closeDriver() method in the DriverSingleton class.
    * Created the DropDownMenu class in which you need to implement the click() method.
    * Refactored folders.

---
+++ src/main/java/model/TopNavBar.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TopNavBar {
+    private WebElement contactUsLink;
+    private WebElement accountLink;
+    private ArrayList<WebElement> myAccountLinks;
+}
--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,24 +1,26 @@
-package page;
+package pages;
 
 import driver.DriverSingleton;
+import model.CategoryMenu;
+import model.Header;
+import model.TopNavBar;
 import org.openqa.selenium.WebDriver;
 
-public abstract class AbstractBasePage {
-    protected WebDriver driver;
-    private String pageAddress;
+public abstract class BasePage {
     protected final int WAIT_TIMEOUT_SECONDS = 10;
+    private String pageAddress;
     public TopNavBar topNavBar;
     public Header header;
     public CategoryMenu categoryMenu;
 
 
-    protected AbstractBasePage(WebDriver driver) {
-        this.driver = driver;
+    protected BasePage() {
+
     }
 
 
     public void openPage() {
-        DriverSingleton.driver.get(this.pageAddress);
+        DriverSingleton.getDriver().get(this.pageAddress);
     }
 
 }
