9b0288c927421153752c95ef5c9761c2e059f1fd
TolkachDima <tolkach.dima64@gmail.com>
2020-12-18 22:55:24 +0300

    Refactoring
    * Created class CategoryPage, ProductItems, realised ProductActions

--- src/main/java/model/ProductItem.java
+++ src/main/java/model/ProductItem.java
@@ -1,4 +1,48 @@
 package model;
 
-public class ProductItem {
+import org.openqa.selenium.support.FindBy;
+import ru.yandex.qatools.htmlelements.element.Button;
+import ru.yandex.qatools.htmlelements.element.HtmlElement;
+import ru.yandex.qatools.htmlelements.element.Link;
+import ru.yandex.qatools.htmlelements.element.TextBlock;
+@FindBy(className = "product-thumb")
+public class ProductItem extends HtmlElement {
+    @FindBy(className = "price")
+    private TextBlock price;
+
+    @FindBy(className = "img-responsive")
+    public Link productImage;
+    @FindBy(xpath = "//*[@class='caption']/h4/a")
+    public Link productName;
+    @FindBy(xpath = "//*[@class='price']")
+    public Double productPrice;
+    @FindBy(xpath = "//*[@id='price-tax']")
+    public Double productTax;
+    @FindBy(xpath = "//*[@class='button-group']/button[1]")
+    public Button addToCartButton;
+    @FindBy(xpath = "//*[@class='button-group']/button[2]")
+    public Button addToWishListButton;
+    @FindBy(xpath = "//*[@class='button-group']/button[3]")
+    public Button addToCompareButton;
+
+    public void clickAddToCart(){
+        addToCartButton.click();
+    }
+
+    public void clickAddToWishList(){
+        addToWishListButton.click();
+    }
+
+    public void clickAddToComparePage(){
+        addToCompareButton.click();
+    }
+
+    public String getImage(){
+        return productImage.getText();
+    }
+
+    public String getPrice(){
+        return price.getText();
+    }
+
 }
---
+++ src/main/java/pages/CategoryPage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import driver.Driver;
+import model.ProductActions;
+import model.ProductItem;
+
+import java.util.List;
+
+public class CategoryPage extends BasePage{
+    private ProductActions productActions;
+    private List<ProductItem> productItemList;
+
+    public CategoryPage(Driver driver) {
+        super(driver);
+    }
+
+    public ProductActions getProductActions() {
+        return productActions;
+    }
+}
+
