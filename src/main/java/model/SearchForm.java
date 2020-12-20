2f9ce942f5c08305812208c2094c5bf61d2ff595
Diana <olkdi@outlook.com>
2020-12-20 23:16:33 +0200

    Clean SearchForm Class

--- src/main/java/model/SearchForm.java
+++ src/main/java/model/SearchForm.java
@@ -1,32 +1,23 @@
 package model;
 
 import org.openqa.selenium.support.FindBy;
-
-import driver.Driver;
-import pages.SearchPage;
-import ru.yandex.qatools.htmlelements.annotations.Name;
 import ru.yandex.qatools.htmlelements.element.Button;
 import ru.yandex.qatools.htmlelements.element.CheckBox;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 import ru.yandex.qatools.htmlelements.element.TextInput;
 
-@Name("Search form")
 @FindBy(id = "content")
 public class SearchForm extends HtmlElement {
 	
-    @Name("Search request input")
     @FindBy(id = "input-search")
     private TextInput searchField;
     
-    @Name("Search in subcategories checkbox")
     @FindBy(name = "sub_category")
     private CheckBox subcatCheckbox;
     
-    @Name("Search in product descriptions checkbox")
     @FindBy(name = "description")
     private CheckBox descriptionCheckbox;
     
-    @Name("Search button")
     @FindBy(id = "button-search") 
     private Button searchButton;
 
