4408fd6c1dece606d2e2b45534258142b894bd9f
Diana <olkdi@outlook.com>
2020-12-22 01:49:08 +0200

    Create Test Case Classes for ContactUsPage TC_DP194_55 TC_DP194_56 TC_DP194_57 TC_DP194_69 TC_DP194_70 TC_DP194_71
    Add csv files for tests
    ContactFormValidEnquiry.csv
    ContactFormInvalidEnquiry.csv
    ContactFormValidName.csv
    ContactFormInvalidName.csv
    ContactFormValidEmail.csv
    ContactFormInvalidEmail.csv
    
    Add description to test cases
    
    Add new fields and methods to ContactUsPage Class
    
    Add asserts to ContactPageStep Class

--- src/main/java/pages/ContactUsPage.java
+++ src/main/java/pages/ContactUsPage.java
@@ -5,41 +5,55 @@ import org.openqa.selenium.support.FindBy;
 import driver.Driver;
 import ru.yandex.qatools.htmlelements.element.Button;
 import ru.yandex.qatools.htmlelements.element.Link;
+import ru.yandex.qatools.htmlelements.element.TextBlock;
 import ru.yandex.qatools.htmlelements.element.TextInput;
 
 public class ContactUsPage extends BasePage {
-	@FindBy(id = "input-name")
-	private TextInput nameField;
-	@FindBy(id = "input-email")
-	private TextInput emailField;
-	@FindBy(id = "input-enquiry")
-	private TextInput enquiryField;
-	@FindBy(xpath = "//*[@class='btn btn-primary']")
-	private Button submit;
-	@FindBy(xpath = "//*[@class='btn btn-primary' and text()='Continue']")
-	private Button continueButton;
-	
-	public ContactUsPage(Driver driver) {
-		super(driver);
-	}
-	
-	public void fillNameField(String name) {
-		nameField.sendKeys(name);
-	}
-	
-	public void fillEmailField(String email) {
-		emailField.sendKeys(email);
-	}
-	
-	public void fillEnquiryField(String enquiry) {
-		enquiryField.sendKeys(enquiry);
-	}
-	
-	public void clickSubmitButton() {
-		submit.click();
-	}
-	
-	public Button getcontinueButton() {
-		return this.continueButton;
-	}
+
+    @FindBy(id = "input-name")
+    private TextInput nameField;
+    @FindBy(id = "input-email")
+    private TextInput emailField;
+    @FindBy(id = "input-enquiry")
+    private TextInput enquiryField;
+    @FindBy(xpath = "//*[@class='btn btn-primary']")
+    private Button submit;
+    @FindBy(xpath = "//*[@class='text-danger' and text()='Name must be between 3 and 32 characters!']")
+    private TextBlock nameErrorMsg;
+    @FindBy(xpath = "//*[@class='text-danger' and text()='E-Mail Address does not appear to be valid!']")
+    private TextBlock emailErrorMsg;
+    @FindBy(xpath = "//*[@class='text-danger' and text()='Enquiry must be between 10 and 3000 characters!']")
+    private TextBlock enquiryErrorMsg;
+
+    public ContactUsPage(Driver driver) {
+        super(driver);
+    }
+
+    public String getNameFieldText() { return nameField.getText(); }
+
+    public String getEmailFieldText() { return emailField.getText(); }
+
+    public String getEnquiryFieldText() { return enquiryField.getAttribute("value"); }
+
+    public void fillNameField(String name) { nameField.sendKeys(name); }
+
+    public void fillEmailField(String email) { emailField.sendKeys(email); }
+
+    public void fillEnquiryField(String enquiry) { enquiryField.sendKeys(enquiry); }
+
+    public void clickSubmitButton() {
+        submit.click();
+    }
+
+    public boolean isNameErrorMsgExist() {
+        return this.nameErrorMsg.exists();
+    }
+
+    public boolean isEmailErrorMsgExist() {
+        return this.emailErrorMsg.exists();
+    }
+
+    public boolean isEnquiryErrorMsgExist() {
+        return this.enquiryErrorMsg.exists();
+    }
 }
--- src/test/java/TC_DP194_71.java
+++ src/test/java/TC_DP194_71.java
@@ -2,12 +2,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import steps.ContactPageStep;
 import steps.HomePageStep;
-//Contact Us Page: Validation the "Contact" form with valid data
-public class TC_DP194_54 extends CommonConditionTest {
+
+//Contact Us Page: Validation the "Enquiry" field of the "Contact" form with valid data
+public class TC_DP194_71 extends CommonConditionTest {
 	String successPageUrl = "http://34.121.117.87/index.php?route=information/contact/success";
 	@ParameterizedTest
-	@CsvFileSource(resources = "/ContactFormValidValues.csv", numLinesToSkip = 1)
-	public void TCDP194_54(String name, String email, String enquiry) {
+	@CsvFileSource(resources = "/ContactFormValidEnquiry.csv", numLinesToSkip = 1)
+	public void TCDP194_71(String name, String email, String enquiry) {
 		HomePageStep homePageStep = new HomePageStep(this.driver);
 		ContactPageStep contactPageStep = homePageStep.clickContactUsLink();
 		contactPageStep.fillNameField(name)
