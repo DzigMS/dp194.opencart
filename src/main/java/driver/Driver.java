ecc5c663b81c49358cfc633b9440580fdbe32ffc
nikomi111 <uselessmouthbest1@gmail.com>
2020-12-21 16:58:47 +0200

    Refactoring Driver class Add support of Edge, Opera browsers Add "acceptInsecureCerts" to capability of every driver type

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -1,9 +1,16 @@
 package driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,19 +26,35 @@ public class Driver {
     }
 
     public static Driver configureDriver(String nameDriver) {
+        MutableCapabilities capabilities = new MutableCapabilities();
+        capabilities.setCapability("acceptInsecureCerts", true);
+
         WebDriver driver;
         switch (nameDriver.toLowerCase()) {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
+
+                driver = new FirefoxDriver(new FirefoxOptions(capabilities));
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+
+                driver = new EdgeDriver(new EdgeOptions().merge(capabilities));
+                break;
+            case "opera":
+                WebDriverManager.operadriver().setup();
+
+                driver = new OperaDriver(new OperaOptions().merge(capabilities));
                 break;
             case "chrome":
             default:
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+
+                driver = new ChromeDriver(new ChromeOptions().merge(capabilities));
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
         return new Driver(driver);
     }
 
