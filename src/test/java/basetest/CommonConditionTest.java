57cdca043c1be670eca660de7afb5f56993dfe2d
Tbotny <tbotny@gmail.com>
2020-12-29 18:54:42 +0200

    Test

--- src/test/java/basetest/CommonConditionTest.java
+++ src/test/java/basetest/CommonConditionTest.java
@@ -4,6 +4,10 @@ import constants.PagesURL;
 import driver.Driver;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import steps.HomePageStep;
+import steps.SearchPageStep;
 
 public class CommonConditionTest {
     protected Driver driver;
@@ -12,6 +16,16 @@ public class CommonConditionTest {
     public void startTest() {
         driver = Driver.configureDriver("chrome");
         driver.openUrl(PagesURL.HOME_PAGE_URL);
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/ShowValuesSearchPage.csv", numLinesToSkip = 1)
+    public void TCDP194_29(int index, int maxValue) {
+        HomePageStep homePageStep = new HomePageStep(this.driver);
+        SearchPageStep searchPageStep = homePageStep.fillSearchField("d").clickSearchButton();
+        searchPageStep.markDescriptionCheckbox()
+                .clickSearchButton()
+                .selectShowValue(index)
+                .verifyQuantity(maxValue);
     }
 
     @AfterEach
