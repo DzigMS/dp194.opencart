a989dc21822e7135da858b4cd6c9691b57f4863b
TolkachDima <tolkach.dima64@gmail.com>
2020-12-29 19:26:42 +0300

    Solve conflict

--- src/main/java/steps/HomePageStep.java
+++ src/main/java/steps/HomePageStep.java
@@ -48,22 +48,6 @@ public class HomePageStep extends BaseStep {
     }
 
 
-        public CheckoutPageAsGuestStep clickOnCheckoutLinkAsGuest() {
-            homePage.getTopNavBar().clickCheckoutLink();
-            return new CheckoutPageAsGuestStep(this.getDriver());
-        }
-
-        public CheckoutPageRegisterStep clickOnCheckoutLinkRegister() {
-            homePage.getTopNavBar().clickCheckoutLink();
-            return new CheckoutPageRegisterStep(this.getDriver());
-        }
-
-        public CheckoutPageAsLoggedUserStep clickOnCheckoutLinkLogged() {
-            homePage.getTopNavBar().clickCheckoutLink();
-            return new CheckoutPageAsLoggedUserStep(this.getDriver());
-        }
-
-
 	public RegisterPageStep goToRegisterPage() {
 		this.homePage.getTopNavBar().clickMyAccountDropdown();
 		this.homePage.getTopNavBar().clickRegisterLink();
