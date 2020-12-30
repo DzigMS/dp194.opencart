f126a37ba63b3962a155138dd04fb7b9876a6e30
nikomi111 <uselessmouthbest1@gmail.com>
2020-12-30 09:24:12 +0200

    Refactor Driver class Remove implicitly wait from driver

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -49,7 +49,6 @@ public class Driver {
                 driver = new ChromeDriver(new ChromeOptions().merge(capabilities));
         }
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         return new Driver(driver);
     }
