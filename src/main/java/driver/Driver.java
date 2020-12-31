395f7c9cd6c3aa9ae1964f8ba87ee8599c1d56ad
Tbotny <tbotny@gmail.com>
2020-12-31 12:53:24 +0200

    change name package

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -56,7 +56,7 @@ public class Driver {
 
     public void closeDriver() {
         if (driver != null) {
-            driver.close();
+            driver.quit();
             driver = null;
         }
     }
